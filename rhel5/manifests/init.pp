5aa678ac3fa837a311aa43997f4c5aadc880e789
Robert Brown <rjb@robertjbrown.com>
2012-01-12 23:05:07 -0800

    Initial module structure

---
+++ rhel5/manifests/init.pp
@@ -0,0 +1,57 @@
+class cis-rhel5 {
+
+# 1.1 Filesystem Configuration
+
+# 1.1.1 Separate Partition for /tmp
+
+# 1.1.2 Set nodev option for /tmp partition
+
+# 1.1.3 Set nosuid option for /tmp partition
+
+# 1.1.4 Set noexec option for /tmp partition
+
+# 1.1.5 Create separate partition for /var
+
+# 1.1.6 Bind munt the /var/tmp directory to /tmp
+
+# 1.1.7 Create separate partition for /var/log
+
+# 1.1.8 Create separate partition for /var/log/audit
+
+# 1.1.9 Create separate partition for /home
+
+# 1.1.10 Add nodev option to /home
+
+# 1.1.11 Add nodev option to removable media partitions
+
+# 1.1.12 Add noexec option to removable media partitions
+
+# 1.1.13 Add nosuid option to removable media partitions
+
+# 1.1.14 Add nodev option to /dev/shm partition
+
+# 1.1.15 Add nosuid option to /dev/shm partition
+
+# 1.1.16 Add noexec option to /dev/shm partition
+
+# 1.1.17 Set sticky bit on all world-writable directories
+
+# 1.1.18 Disable mounting of cramfs filesystems
+
+# 1.1.19 Disable mounting of freevxfs filesystems
+
+# 1.1.20 Disable mounting of jffs2 filesystems
+
+# 1.1.21 Disable mounting of hfs filesystems
+
+# 1.1.22 Disable mounting of hfsplus filesystems
+
+# 1.1.23 Disable mounting of squashfs filesystems
+
+# 1.1.24 Disable mounting of udf filesystems
+
+# 1.2 Use the Latest OS Release
+
+# 1.3 Configure Software Updates
+
+}
